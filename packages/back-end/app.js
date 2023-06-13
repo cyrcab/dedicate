63b0efa01698d00c5fbf18541df1eafcbe661e44
cyril cabrolier <cyr.cab@hotmail.fr>
2023-06-13 11:08:15 +0200

    feat(backend): 🎸 move backend file and adding some script in package json

--- packages/back-end/app.js
+++ packages/back-end/app.js
@@ -14,8 +14,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
+app.listen(5001, () => {
+    console.log('Server started on port 5001');
 });
 
 module.exports = app;
