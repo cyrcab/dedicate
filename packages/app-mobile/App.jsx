d0af8b731706e3a701eb2222cf39b196a3d1d572
ClementMendes <clement.mendes@epitech.eu>
2023-06-19 15:55:16 +0200

    feat(mobile-app): 🎸 Adding a theme to the app

--- packages/app-mobile/App.jsx
+++ packages/app-mobile/App.jsx
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Salut Aurélien</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
