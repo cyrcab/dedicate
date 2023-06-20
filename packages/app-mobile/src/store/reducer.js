6b2f89624b6e771dd083eb3eaa112c9f5481e023
ClementMendes <clement.mendes@epitech.eu>
2023-06-20 16:20:56 +0200

    fix(mobile-app): 🐛 resolve lint on app-mobile

--- packages/app-mobile/src/store/reducer.js
+++ packages/app-mobile/src/store/reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from './userReducer'; 
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
--- packages/app-mobile/src/store/userReducer.js
+++ packages/app-mobile/src/store/userReducer.js
@@ -1,15 +1,14 @@
 const initialState = {
-    user: null,
-  };
-  
-  const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'SET_USER_DATA':
-        return { ...state, user: action.payload };
-      default:
-        return state;
-    }
-  };
-  
-  export default userReducer;
-  
+  user: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_USER_DATA':
+      return { ...state, user: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
