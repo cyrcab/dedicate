ac5e5c239a5fb307ba7c1066e325dd2b210bde42
cyril cabrolier <cyr.cab@hotmail.fr>
2023-06-27 17:18:28 +0200

    fix(web-app): 🐛 fix build error beacause of typo in index

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Technology from '../components/Technology'
 import Partenaires from '../components/Partenaires'
 import Vision from '../components/Vision'
