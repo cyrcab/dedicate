3fc375357fb2801a8e38bd4ea96dc1f863985ebb
Alexandre Merigot <alexandre@data-pulse.fr>
2023-07-03 15:30:16 +0200

    feat(website): 🎸 Mise a jour du site pour ajouter de la responsivité

--- packages/website/src/components/Navbar.js
+++ packages/website/src/components/Navbar.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import '../styles/global.css'
+import React, { useState } from 'react';
+import '../styles/global.css';
 
 export default function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navigation">
-        <div className="title">DEDICATE</div>
-        <div className="links">
-            <a href="#technology-section">Technology</a>
-            <a href="#vision-section">Vision</a>
-            <a href="#partenaires-section">Partenaires</a>
-            <a href="#contact-section">contact</a>
-        </div>
-        <div className="adminBtn">Déja Client</div>
-        
+      <div className="title">DEDICATE</div>
+      <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        ☰
+      </div>
+      <div className={`links ${isMenuOpen ? 'active' : ''}`}>
+        <a className='navLink' href="#technology-section" onClick={closeMenu}>Technology</a>
+        <a className='navLink' href="#vision-section" onClick={closeMenu}>Vision</a>
+        <a className='navLink' href="#partenaires-section" onClick={closeMenu}>Partenaires</a>
+        <a className='navLink' href="#contact-section" onClick={closeMenu}>Contact</a>
+        <div className="adminBtn navLink">Déjà Client</div>
+      </div>
     </nav>
-
-  )
+  );
 }
