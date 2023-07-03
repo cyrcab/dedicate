a79ea22ec88529e5884dd31df482b0cc1a2a4377
ClementMendes <clement.mendes@epitech.eu>
2023-07-03 16:19:32 +0200

    feat(mobile-app): 🎸 construction of the home page

--- packages/app-mobile/src/pages/Home.jsx
+++ packages/app-mobile/src/pages/Home.jsx
@@ -1,18 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import EventsCard from '../components/EventsCard';
 
-export default function Home() {
-  
+export default function Home(){ 
+
   return (
-    <View
-      style={{
-        display: "flex",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logo}>DEDICATE</Text>
+        <FontAwesome name="search" size={24} style={styles.searchIcon} />
+      </View>
+      <ScrollView style={styles.eventList}>
+
+          <EventsCard />
+      </ScrollView>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+    marginTop: 30
+  },
+  logo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  searchIcon: {
+    marginLeft: 16,
+  },
+  eventList: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  eventItem: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+});
---
+++ packages/app-mobile/src/pages/ReadMore.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import styles from "./styles";
+
+export default function ReadMore(){
+    return(
+        <View>
+            <View style={styles.ReadMoreHeader}>
+
+            </View>
+        </View>
+    );
+}
