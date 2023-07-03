728db3015fab3b33856070a4f01208b2d8db7971
ClementMendes <clement.mendes@epitech.eu>
2023-07-03 11:43:04 +0200

    feat(mobile-app): 🎸 modify password

--- packages/app-mobile/src/navigation/AppNavigator.jsx
+++ packages/app-mobile/src/navigation/AppNavigator.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Event from "../pages/Event";
 import Settings from "../pages/Settings"
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-
 
 const ProfileStack = () => (
   <Stack.Navigator>
@@ -20,10 +20,23 @@ const ProfileStack = () => (
 
 const AppNavigator = () => (
   <Tab.Navigator 
-  initialRouteName="Home"
-  screenOptions={{
-    headerShown: false,
-  }}
+    initialRouteName="Home"
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Acceuil") {
+          iconName = focused ? "home" : "home-outline";
+        } else if (route.name === "Evénements") {
+          iconName = focused ? "calendar" : "calendar-outline";
+        } else if (route.name === "Profile") {
+          iconName = focused ? "person" : "person-outline";
+        }
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
   >
     <Tab.Screen name="Acceuil" component={Home} />
     <Tab.Screen name="Evénements" component={Event} />
--- packages/app-mobile/src/pages/Home.jsx
+++ packages/app-mobile/src/pages/Home.jsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-paper';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useDispatch, useSelector } from "react-redux";
-import { setSignedIn } from "../store/reducers/reducer";
-
-
-
+import React from "react";
+import { View, Text } from "react-native";
 
 export default function Home() {
-
-    const dispatch = useDispatch();
-
-    function removeAsyncToken(){
-        AsyncStorage.removeItem('token');
-        dispatch(setSignedIn(false));
-    }
-    return (
-        <View style={{display:'flex', height:'100%', alignItems:'center', justifyContent:'center'}}>
-            <Text>Home</Text>
-            <Button
-            onPress={removeAsyncToken}
-            >Logout</Button>
-        </View>
-    );
+  
+  return (
+    <View
+      style={{
+        display: "flex",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Home</Text>
+    </View>
+  );
 }
