2b375df72147bedccc781ca7746cee4fee6ba3fd
ClementMendes <clement.mendes@epitech.eu>
2023-07-04 11:47:10 +0200

    feat(mobile-app): 🎸 list of events/ readmore events / qrcode button

--- packages/app-mobile/src/pages/Home.jsx
+++ packages/app-mobile/src/pages/Home.jsx
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { View, Text, ScrollView, StyleSheet, RefreshControl } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import EventsCard from "../components/EventsCard";
 import { axiosApiInstance } from "../../axios.config";
 import { backendUrl } from "../backendUrl";
+import { Avatar, Button } from "react-native-paper";
 
 export default function Home() {
   const [event, setEvent] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
   function EventInfo() {
     axiosApiInstance.get(backendUrl + "events/next/next").then((response) => {
       setEvent(response.data.data);
+      setRefreshing(false);
     });
   }
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    EventInfo();
+  };
 
   useEffect(() => {
     EventInfo();
   }, []);
-  console.log(event);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.logo}>DEDICATE</Text>
         <FontAwesome name="search" size={24} style={styles.searchIcon} />
       </View>
-      <ScrollView style={styles.eventList}>
-        {event.map((item) => (
-          <EventsCard item={item} />
+      <ScrollView
+        style={styles.eventList}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {event.map((item, index) => (
+          <EventsCard item={item} key={index} />
         ))}
       </ScrollView>
+      <Button
+        icon={"qrcode"}
+        onPress={() => console.log('Button pressed')}
+        mode="contained"
+        style={{ borderRadius: 50, width: 60, height: 62, position:'absolute', bottom:20, left:'41%'}}
+        contentStyle={{ width: 80, height: 60 }}
+        labelStyle={{ fontSize: 40 }}
+      />
     </View>
   );
 }
--- packages/app-mobile/src/pages/ReadMore.jsx
+++ packages/app-mobile/src/pages/ReadMore.jsx
@@ -1,14 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import styles from "./styles";
+import { Avatar, Button } from "react-native-paper";
+import { logo } from "../../assets/oclub.png";
 
-export default function ReadMore({route}){
-    const { event, setEvent } = route.params;
+export default function ReadMore({ route }) {
+  const { event } = route.params;
 
-    return(
-        <View>
-            <View style={styles.ReadMoreHeader}>
-            <Text></Text>
-            </View>
+  return (
+    <ScrollView style={styles.containerReadMore}>
+      <View style={styles.etablissementInformation}>
+        <Avatar.Image size={100} source={require("../../assets/oclub.png")} />
+        <View style={styles.containerEtablissementInformation}>
+          <Text>{event.Etablissement.nom}</Text>
+          <Text>{event.Etablissement.adresse}</Text>
+          <Text>{event.Etablissement.ville}</Text>
         </View>
-    );
+      </View>
+      <View style={styles.readMorePresentation}>
+        <Text style={styles.title}>{event.nom}</Text>
+        <Text style={styles.price}>
+          Prix de base des musiques: {event.prix}€
+        </Text>
+      </View>
+      <Text style={styles.description}>
+        Venez nombreux, super soirée électro !
+      </Text>
+      <View style={styles.musicTypeContainer}>
+        <Text style={styles.musicTypeLabel}>Type de musique :</Text>
+        <Button mode="elevated" style={styles.musicTypeButton}>
+          {event.type}
+        </Button>
+      </View>
+      <Button
+        mode="contained"
+        style={styles.qrCodeButton}
+        onPress={() => console.log('Pressed')}
+      >
+        SCANNEZ LE QR CODE
+      </Button>
+    </ScrollView>
+  );
 }
