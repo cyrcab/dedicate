9f2a0e9b60e8649f7a3d7f78056f48526d0f9dcd
ribcoco <ribcoco@gmail.com>
2023-07-10 11:24:02 +0200

    feat(backend): 🎸 Add enchere and get enchere

--- packages/back-end/routes/enchere.route.js
+++ packages/back-end/routes/enchere.route.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const enchereController = require('../controllers/enchere.controller');
 
 router.post('/', enchereController.vote);
+router.get('/:id', enchereController.getVotes);
 
 module.exports = router;
