7b2a168157d3891c891a5f697eff11deb7a72ce0
Chachafouineuse <aurelien.schmit@outlook.fr>
2023-07-10 14:55:05 +0200

    feat(mobile-app): 🎸 scan qr code v1 added

--- packages/app-mobile/src/components/CodeQR.jsx
+++ packages/app-mobile/src/components/CodeQR.jsx
@@ -1,22 +1,65 @@
-import { Button } from "react-native-paper";
+import React, { useState } from 'react';
+import { View, StyleSheet } from "react-native";
+import { IconButton, Button } from "react-native-paper";
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function CodeQR() {
+  const [scanning, setScanning] = useState(false);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanning(false);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (scanning) {
+    return (
+      <View style={styles.container}>
+        <BarCodeScanner
+          onBarCodeScanned={handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+        <IconButton
+          icon="close"
+          color="white"
+          size={24}
+          onPress={() => setScanning(false)}
+          style={styles.closeIcon}
+        />
+      </View>
+    );
+  }
+
   return (
-    <>
-      <Button
-        icon="qrcode"
-        mode="contained"
-        style={{
-          borderRadius: 50,
-          width: 60,
-          height: 62,
-          position: "absolute",
-          bottom: 20,
-          left: "41%",
-        }}
-        contentStyle={{ width: 80, height: 60 }}
-        labelStyle={{ fontSize: 40 }}
-      />
-    </>
+    <Button
+      icon="qrcode"
+      mode="contained"
+      style={{
+        borderRadius: 50,
+        width: 60,
+        height: 62,
+        position: "absolute",
+        bottom: 20,
+        left: "41%",
+      }}
+      contentStyle={{ width: 80, height: 60 }}
+      labelStyle={{ fontSize: 40 }}
+      onPress={() => setScanning(true)}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 50,
+    right: 20,
+  },
+});
