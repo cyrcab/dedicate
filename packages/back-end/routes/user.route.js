e08194d69fd0cd02522e7662ffe5d0d6d35f4f52
ribcoco <ribcoco@gmail.com>
2023-07-11 12:12:34 +0200

    feat(backend): 🎸 Swagger Begin

--- packages/back-end/routes/user.route.js
+++ packages/back-end/routes/user.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 const { authTokenAdmin, checkMe } = require('../middleware/auth.middleware');
+/* eslint-disable */
 /**
  * @swagger
  * tags:
