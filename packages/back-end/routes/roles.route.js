dacba9ea5832eaedcebabb213498adad583cedb6
cyril cabrolier <cyr.cab@hotmail.fr>
2023-07-11 17:31:56 +0200

    fix(backend): 🐛 fix prettier and eslint bug

--- packages/back-end/routes/roles.route.js
+++ packages/back-end/routes/roles.route.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const roleController = require('../controllers/roles.controller');
 const { authTokenAdmin } = require('../middleware/auth.middleware');
-/* eslint-disable */
 /**
  * @swagger
  * tags:
