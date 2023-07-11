697bd3bbdcfdd15c8447ae6eb355b813c9850ae0
ribcoco <ribcoco@gmail.com>
2023-07-11 14:16:59 +0200

    feat(backend): 🎸 add Photo musique and eventActif User

--- packages/back-end/controllers/event.controller.js
+++ packages/back-end/controllers/event.controller.js
@@ -334,6 +334,15 @@ module.exports.addUserToEvent = async (req, res) => {
         .status(400)
         .json({ message: "Cet utilisateur est déjà dans l'événement" });
     }
+    await prisma.user.update({
+      where: {
+        id: parseInt(idUser, 10),
+      },
+      data: {
+        eventActif: idEvent
+      }
+    })
+
     const userEvent = await prisma.event.update({
       where: {
         id: parseInt(idEvent, 10),
---
+++ packages/back-end/prisma/migrations/20230711110459_v2/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `musique` ADD COLUMN `album` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `eventActif` VARCHAR(191) NULL;
