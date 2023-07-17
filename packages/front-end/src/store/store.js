45bf7246d210a30a3e7af3ecfab19d00cd1db8a8
Alexandre Merigot <alexandre@data-pulse.fr>
2023-07-17 16:23:03 +0200

    feat(web-app): 🎸 Création page Login/register

--- packages/front-end/src/store/store.js
+++ packages/front-end/src/store/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import auth from './reducer/reducer';
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducer/reducer'
 
 export default configureStore({
-  reducer: { auth },
-});
+  reducer: { rootReducer },
+})
