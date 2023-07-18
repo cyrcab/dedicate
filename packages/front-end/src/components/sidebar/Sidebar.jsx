ed5c15dde975bd51f5e95fb675030b2a7fe57635
cyril cabrolier <cyr.cab@hotmail.fr>
2023-07-18 11:51:19 +0200

    style(web-app): 💄 theming app + starting style side bar

--- packages/front-end/src/components/sidebar/Sidebar.jsx
+++ packages/front-end/src/components/sidebar/Sidebar.jsx
@@ -1,45 +1,66 @@
 import React from 'react';
+import { Box, Typography, CardMedia } from '@mui/material';
 import './sidebar.css';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import EventIcon from '@mui/icons-material/Event';
 import QueueMusicIcon from '@mui/icons-material/QueueMusic';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import logo from '../../assets/logo.png';
+import logo from './assets/logo.png';
 
 export default function Sidebar() {
   return (
-    <div className='dashboard'>
-      <div className='logo'>
-        <img src={logo} className='dashboard_logo' />
-        <div className='logoName'>DEDICATE</div>
-      </div>
+    <Box
+      sx={{
+        height: '100vh',
+        backgroundColor: '#ffffff',
+        borderRight: '1px solid #E6E6E6',
+      }}
+    >
+      <Box
+        sx={{
+          height: '10%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '0 20px',
+          flexDirection: 'column',
+        }}
+      >
+        <CardMedia
+          image={logo}
+          sx={{
+            height: '40px',
+            width: '40px',
+          }}
+        />
+        <Typography variant="h4">DEDICATE</Typography>
+      </Box>
 
-      <div className='dashboard_content'>
-        <div className='dashboard_list'>
-          <div className='dashboard_unit'>
+      <Box>
+        <Box>
+          <Box>
             <LeaderboardIcon />
-            <a href='/'>Dashboard</a>
-          </div>
-          <div className='dashboard_unit'>
+            <a href="/">Dashboard</a>
+          </Box>
+          <Box>
             <EventIcon />
-            <a href='/myEvents'>My Events</a>
-          </div>
-          <div className='dashboard_unit'>
+            <a href="/myEvents">My Events</a>
+          </Box>
+          <Box>
             <QueueMusicIcon />
-            <a href='/playlist'>Playlist</a>
-          </div>
-          <div className='dashboard_unit'>
+            <a href="/playlist">Playlist</a>
+          </Box>
+          <Box>
             <AccountCircleIcon />
-            <a href='/users'>Users</a>
-          </div>
-          <div className='dashboard_unit'>
+            <a href="/users">Users</a>
+          </Box>
+          <Box>
             <SettingsIcon />
-            <a href='/settings'>Settings</a>
-          </div>
-
-        </div>
-      </div>
-    </div>
+            <a href="/settings">Settings</a>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
