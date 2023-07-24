1a37053f86dfcf1f791fa97ba2b7845aafda4b9a
cyril cabrolier <cyr.cab@hotmail.fr>
2023-07-24 11:18:06 +0200

    refactor(web-app): 💡 refactor sidebar + adding component for links in sidebar

--- packages/front-end/src/components/sidebar/Sidebar.jsx
+++ packages/front-end/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Box, Typography, CardMedia } from '@mui/material';
 import './sidebar.css';
+import { useLocation } from 'react-router-dom';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import EventIcon from '@mui/icons-material/Event';
 import QueueMusicIcon from '@mui/icons-material/QueueMusic';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import logo from './assets/logo.png';
+import MySidebarLink from './components/MySidebarLink';
 
 export default function Sidebar() {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    if (location.pathname === path) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <Box
       sx={{
@@ -37,29 +48,46 @@ export default function Sidebar() {
         <Typography variant="h4">DEDICATE</Typography>
       </Box>
 
-      <Box>
-        <Box>
-          <Box>
-            <LeaderboardIcon />
-            <a href="/">Dashboard</a>
-          </Box>
-          <Box>
-            <EventIcon />
-            <a href="/myEvents">My Events</a>
-          </Box>
-          <Box>
-            <QueueMusicIcon />
-            <a href="/playlist">Playlist</a>
-          </Box>
-          <Box>
-            <AccountCircleIcon />
-            <a href="/users">Users</a>
-          </Box>
-          <Box>
-            <SettingsIcon />
-            <a href="/settings">Settings</a>
-          </Box>
-        </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height: '50%',
+          marginTop: '2rem',
+          padding: '0 20px',
+        }}
+      >
+        <MySidebarLink
+          isActive={isActive('/')}
+          path={'/'}
+          icon={<LeaderboardIcon />}
+          pathName={'Accueil'}
+        />
+        <MySidebarLink
+          isActive={isActive('/myEvents')}
+          path={'/myEvents'}
+          icon={<EventIcon />}
+          pathName={'Événements'}
+        />
+        <MySidebarLink
+          isActive={isActive('/playlist')}
+          path={'/playlist'}
+          icon={<QueueMusicIcon />}
+          pathName={'Playlist'}
+        />
+        <MySidebarLink
+          isActive={isActive('/users')}
+          path={'/users'}
+          icon={<AccountCircleIcon />}
+          pathName={'Utilisateurs'}
+        />
+        <MySidebarLink
+          isActive={isActive('/settings')}
+          path={'/settings'}
+          icon={<SettingsIcon />}
+          pathName={'Paramètres'}
+        />
       </Box>
     </Box>
   );
