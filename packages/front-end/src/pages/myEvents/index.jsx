ba5a83458437c848c53e3fece33e86f1ac3a58a4
cyril cabrolier <cyr.cab@hotmail.fr>
2023-08-01 15:14:16 +0200

    refactor(web-app): 💡 first version of generic table responsive

--- packages/front-end/src/pages/myEvents/index.jsx
+++ packages/front-end/src/pages/myEvents/index.jsx
@@ -1,22 +1,106 @@
 /* eslint-disable indent */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style/myEvents.css';
-import Card from '@mui/material/Card';
+import { IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import InfoIcon from '@mui/icons-material/Info';
 import { Link } from 'react-router-dom';
-import Events from './component/Events';
+import { useSelector } from 'react-redux';
+import Table from '../../components/Table';
+import { backendUrl } from '../../backendUrl';
+import { axiosApiInstance } from '../../axios.config';
 
 export default function MyEvents() {
+  const [rows, setRows] = useState([]);
+
+  const idCompany = useSelector((state) => state.auth.user.idEtablissement);
+
+  useEffect(() => {
+    axiosApiInstance
+      .get(`${backendUrl}events/${idCompany}`)
+      .then((response) => {
+        const formattedData = response.data.data.map((item) => ({
+          id: item.id,
+          nom: item.nom,
+          type: item.type,
+          description: item.description,
+          lieu: item.lieu,
+          nbSlots: item.nbSlots,
+          date: item.date,
+          prix: item.prix,
+        }));
+        setRows(formattedData);
+      })
+      .catch((error) => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+  const columns = [
+    {
+      field: 'nom',
+      headerName: 'Event',
+    },
+    {
+      field: 'lieu',
+      headerName: 'Lieu',
+    },
+    {
+      field: 'type',
+      headerName: 'Type',
+    },
+    {
+      field: 'description',
+      headerName: 'Description',
+      sortable: false,
+    },
+    {
+      field: 'date',
+      headerName: 'Date',
+    },
+    {
+      field: 'prix',
+      headerName: 'Prix',
+    },
+    {
+      field: 'nbSlots',
+      headerName: 'Slots',
+    },
+    {
+      field: 'seeMore',
+      headerName: 'See More',
+      renderCell: (params) => (
+        <Link to={`/events/${params.row.id}`}>
+          <IconButton>
+            <InfoIcon />
+          </IconButton>
+        </Link>
+      ),
+    },
+    {
+      field: 'edit',
+      headerName: 'Edit',
+      sortable: false,
+      renderCell: (params) => {
+        const eventDate = new Date(params.row.date);
+        const currentDate = new Date();
+        const isEventPassed = eventDate < currentDate;
+        return (
+          <Link to={isEventPassed ? '#' : `/events/edit/${params.row.id}`}>
+            {isEventPassed ? 'Fini' : <EditIcon />}
+          </Link>
+        );
+      },
+    },
+  ];
   return (
-    <div className="myEvents">
-      <Link to={'/events/create'}>
-        <Card
-          sx={{ m: 2, backgroundColor: 'rgba(202, 69, 186, 0.5)' }}
-          className="createEvent"
-        >
-          Créer un évènement
-        </Card>
-      </Link>
-      <Events />
-    </div>
+    <Table
+      rows={rows}
+      columns={columns}
+      headerButton={{
+        title: 'Ajouter un événement',
+        link: '/events/create',
+      }}
+    />
   );
 }
--- packages/front-end/src/pages/users/index.jsx
+++ packages/front-end/src/pages/users/index.jsx
@@ -1,78 +1,51 @@
 /* eslint-disable indent */
 import React from 'react';
 import './style/users.css';
-import { DataGrid } from '@mui/x-data-grid';
-import Card from '@mui/material/Card';
+import Table from '../../components/Table';
 
 const columns = [
   {
-    field: 'id', headerName: 'ID', width: 70,
-  },
-  {
-    field: 'EventName', headerName: 'Event', width: 130,
+    field: 'EventName',
+    headerName: 'Event',
   },
   {
     field: 'Status',
     headerName: 'Status',
-    width: 90,
   },
   {
     field: 'Style',
     headerName: 'Style',
-    width: 180,
   },
   {
     field: 'Description',
     headerName: 'Description',
     sortable: false,
-    width: 400,
   },
 ];
-    const rows = [
-    {
-        id: 1,
-        EventName: 'TechnoMix',
-        Status: 'En Cours',
-        Style: 'Techno / House / Electro',
-        Description: 'aze;jazhba;jhavcb',
-    },
-    {
-        id: 2,
-        EventName: 'TechnoMix',
-        Status: 'En Cours',
-        Style: 'Techno / House / Electro',
-        Description: 'Lorem bk lijazljbdc ',
-    },
-    {
-        id: 3,
-        EventName: 'TechnoMix',
-        Status: 'En Cours',
-        Style: 'Techno / House / Electro',
-        Description: 'Lorem bk lijazljbdc ',
-    },
-    ];
+const rows = [
+  {
+    id: 1,
+    EventName: 'TechnoMix',
+    Status: 'En Cours',
+    Style: 'Techno / House / Electro',
+    Description: 'aze;jazhba;jhavcb',
+  },
+  {
+    id: 2,
+    EventName: 'TechnoMix',
+    Status: 'En Cours',
+    Style: 'Techno / House / Electro',
+    Description: 'Lorem bk lijazljbdc ',
+  },
+  {
+    id: 3,
+    EventName: 'TechnoMix',
+    Status: 'En Cours',
+    Style: 'Techno / House / Electro',
+    Description: 'Lorem bk lijazljbdc ',
+  },
+];
 
 export default function Users() {
-  return (
-    <div className='myEvents'>
-      <Card sx={{ m: 2, backgroundColor: 'rgba(202, 69, 186, 0.5)' }} className='createEvent'>
-        Ajouter un utilisateur
-      </Card>
-      <Card sx={{ m: 2 }}>
-        <div style={{ width: '100%' }}>
-        <DataGrid
-            rows={rows}
-            columns={columns}
-            headerClassName="custom-header"
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 10 },
-              },
-            }}
-            pageSizeOptions={[10, 30]}
-          />
-        </div>
-      </Card>
-    </div>
-  );
+  return <Table rows={rows} columns={columns} />;
 }
