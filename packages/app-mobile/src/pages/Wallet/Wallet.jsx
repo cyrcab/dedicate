a9881a572397583ba7dfc915e2d084e79e575103
Chachafouineuse <aurelien.schmit@outlook.fr>
2023-08-12 19:55:00 +0200

    feat(mobile-app): 🎸 wallet section added

--- packages/app-mobile/src/pages/Wallet/Wallet.jsx
+++ packages/app-mobile/src/pages/Wallet/Wallet.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
-import styles from "./styles";
+import { axiosApiInstance } from "../../../axios.config";
+import { backendUrl } from "../../backendUrl";
+import { useIsFocused } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import styles from "../styles";
 
 export default function Wallet({ navigation }) {
+    const [dediCoins, setDediCoins] = useState(0);
+    const isFocused = useIsFocused();
+    const userId = useSelector(state => state.auth.userId);
+
+    function UserDedicoins() {
+        axiosApiInstance.get(backendUrl + "users/dedicoins/" + userId).then((response) => {
+            setDediCoins(response.data.data);
+        });
+    }
+
+    useEffect(() => {
+        UserDedicoins();
+    }, [isFocused]);
+
 
     return (
         <View style={styles.containerHome}>
             <View style={styles.headerHome}>
                 <Text style={styles.logoHome}>Wallet</Text>
+                <Text style={styles.logoHome}>Dedicoins: {dediCoins}</Text>
             </View>
             <View style={styles.centerView}>
                 <Button
--- packages/back-end/controllers/transaction.controller.js
+++ packages/back-end/controllers/transaction.controller.js
@@ -1,15 +1,64 @@
-// const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// module.exports.createTransaction = async (req, res) => {
-//     // code pour créer une nouvelle transaction
-// };
+module.exports.createTransaction = async (req, res) => {
+    const { userId, paymentMethodId, amount } = req.body;
 
-// module.exports.getTransaction = async (req, res) => {
-//     // code pour obtenir une transaction spécifique
-// };
+    // Vérifier si l'utilisateur a au moins un moyen de paiement
+    const userPaymentMethods = await prisma.paymentMethod.findMany({ where: { userId } });
+    if (userPaymentMethods.length === 0) {
+        return res.status(400).json({ error: "L'utilisateur n'a aucun moyen de paiement enregistré" });
+    }
 
-// module.exports.getUserTransactions = async (req, res) => {
-//     // code pour obtenir toutes les transactions d'un utilisateur spécifique
-// };
+    // Vérifier si le moyen de paiement spécifié est valide
+    const paymentMethod = userPaymentMethods.find(method => method.id === paymentMethodId);
+    if (!paymentMethod) {
+        return res.status(400).json({ error: "Le moyen de paiement spécifié est invalide" });
+    }
+
+    // Créer une nouvelle transaction
+    const transaction = await prisma.transaction.create({
+        data: {
+            userId,
+            amount,
+            type: 'purchase',
+            date: new Date()
+        }
+    });
+
+    // Récupérer l'utilisateur
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+        return res.status(400).json({ error: "L'utilisateur spécifié n'existe pas" });
+    }
+
+    // Mettre à jour les dediCoins de l'utilisateur
+    const updatedUser = await prisma.user.update({
+        where: { id: userId },
+        data: { dediCoins: user.dediCoins + amount }
+    });
+
+    // Retourner la transaction et le nouvel état de l'utilisateur
+    return res.json({ transaction, user: updatedUser });
+};
+
+module.exports.getTransaction = async (req, res) => {
+    const { id } = req.params;
+
+    const transaction = await prisma.transaction.findUnique({ where: { id: Number(id) } });
+
+    if (!transaction) {
+        return res.status(404).json({ error: "Transaction non trouvée" });
+    }
+
+    return res.json(transaction);
+};
+
+module.exports.getUserTransactions = async (req, res) => {
+    const { userId } = req.params;
+
+    const transactions = await prisma.transaction.findMany({ where: { userId: Number(userId) } });
+
+    return res.json(transactions);
+};
