08cc08564ca979caf44a7d46b4925829a579e394
ClementMendes <clement.mendes@epitech.eu>
2023-09-04 15:59:47 +0200

    style(mobile-app): 💄 styling the app

--- packages/app-mobile/src/pages/Settings.jsx
+++ packages/app-mobile/src/pages/Settings.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Keyboard,
@@ -6,33 +6,34 @@ import {
   TouchableWithoutFeedback,
   KeyboardAvoidingView,
   Platform,
-} from "react-native";
+  ImageBackground
+} from 'react-native';
 import {
   Avatar,
   TextInput,
   Snackbar,
   HelperText,
   Button,
-} from "react-native-paper";
-import { axiosApiInstance } from "../../axios.config";
-import { backendUrl } from "../backendUrl";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useDispatch } from "react-redux";
-import { setSignedIn } from "../store/reducers/reducer";
-import styles from "./styles";
+} from 'react-native-paper';
+import { axiosApiInstance } from '../../axios.config';
+import { backendUrl } from '../backendUrl';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useDispatch } from 'react-redux';
+import { setSignedIn } from '../store/reducers/reducer';
+import styles from './styles';
 
 export default function Settings({ route }) {
   const { user, setUser } = route.params;
   const [nom, setNom] = useState(user.nom);
   const [prenom, setPrenom] = useState(user.prenom);
   const [email, setEmail] = useState(user.mail);
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [visible, setVisible] = useState(false);
-  const [messageError, setMessageError] = useState("");
+  const [messageError, setMessageError] = useState('');
   const [selectedInput, setSelectedInput] = useState(null);
-  const [passwordError, setPasswordError] = useState("");
-  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
   const onDismissSnackBar = () => setVisible(false);
 
@@ -49,7 +50,7 @@ export default function Settings({ route }) {
     }
 
     axiosApiInstance
-      .put(backendUrl + "users/" + user?.id, updatedUser)
+      .put(backendUrl + 'users/' + user?.id, updatedUser)
       .then((response) => {
         setMessageError(response.data.message);
         setVisible(true);
@@ -70,17 +71,17 @@ export default function Settings({ route }) {
 
   const handleSavePassword = () => {
     if (password !== confirmPassword) {
-      setConfirmPasswordError("Les mots de passe ne correspondent pas");
+      setConfirmPasswordError('Les mots de passe ne correspondent pas');
       return;
     }
     if (password.length === 0) {
-      setPasswordError("Le mot de passe doit contenir au moins un caractère");
+      setPasswordError('Le mot de passe doit contenir au moins un caractère');
       return;
     }
-    setPasswordError("");
-    setConfirmPasswordError("");
+    setPasswordError('');
+    setConfirmPasswordError('');
     axiosApiInstance
-      .put(backendUrl + "users/password/" + user?.id, { password: password })
+      .put(backendUrl + 'users/password/' + user?.id, { password: password })
       .then((response) => {
         setMessageError(response.data.message);
         setVisible(true);
@@ -101,8 +102,8 @@ export default function Settings({ route }) {
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
-      "keyboardDidShow",
-      () => setSelectedInput(null)
+      'keyboardDidShow',
+      () => setSelectedInput(null),
     );
 
     return () => {
@@ -112,116 +113,123 @@ export default function Settings({ route }) {
 
   const handleConfirmPasswordChange = (text) => {
     setConfirmPassword(text);
-    setConfirmPasswordError("");
+    setConfirmPasswordError('');
   };
 
   const handlePasswordChange = (text) => {
     setPassword(text);
-    setPasswordError("");
+    setPasswordError('');
   };
 
   const dispatch = useDispatch();
 
   function removeAsyncToken() {
-    AsyncStorage.removeItem("token");
+    AsyncStorage.removeItem('token');
     dispatch(setSignedIn(false));
   }
 
   return (
-    <TouchableWithoutFeedback onPress={handleOutsidePress}>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : null}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 0}
-      >
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-          <View style={{ flex: 1, padding: 16 }}>
-            <View style={styles.centerView}>
-              <Avatar.Image
-                size={100}
-                source={require("../../assets/profile.jpg")}
-              />
-            </View>
-            <TextInput
-              label="Nom"
-              value={nom}
-              onChangeText={(text) => setNom(text)}
-              mode="outlined"
-              onFocus={() => handleInputFocus("nom")}
-              style={selectedInput === "nom" ? { borderColor: "blue" } : null}
-            />
-            <TextInput
-              label="Prénom"
-              value={prenom}
-              onChangeText={(text) => setPrenom(text)}
-              mode="outlined"
-              onFocus={() => handleInputFocus("prenom")}
-              style={
-                selectedInput === "prenom" ? { borderColor: "blue" } : null
-              }
-            />
-            <TextInput
-              label="Email"
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-              mode="outlined"
-              onFocus={() => handleInputFocus("email")}
-              style={selectedInput === "email" ? { borderColor: "blue" } : null}
-            />
-            <View style={styles.centerButton}>
-              <Button onPress={handleSaveChanges}>
-                Enregistrer les modifications
-              </Button>
-            </View>
-
-            <TextInput
-              label="Nouveau mot de passe"
-              value={password}
-              onChangeText={handlePasswordChange}
-              mode="outlined"
-              secureTextEntry
-            />
-            {passwordError && (
-              <HelperText type="error" visible={passwordError !== ""}>
-                {passwordError}
-              </HelperText>
-            )}
-
-            <TextInput
-              label="Confirmez votre mot de passe"
-              value={confirmPassword}
-              onChangeText={handleConfirmPasswordChange}
-              mode="outlined"
-              secureTextEntry
-            />
-            {confirmPasswordError && (
-              <HelperText type="error" visible={confirmPasswordError !== ""}>
-                {confirmPasswordError}
-              </HelperText>
-            )}
-            <View style={styles.centerButton}>
-              <Button
-                onPress={handleSavePassword}
-                style={styles.settingsButton}
-              >
-                Enregistrer le nouveau mot de passe
-              </Button>
-            </View>
-            <View style={styles.centerButton}>
-              <Button onPress={removeAsyncToken} mode="contained">
-                Logout
-              </Button>
-            </View>
-          </View>
-        </ScrollView>
-        <Snackbar
-          wrapperStyle={{ top: 0 }}
-          visible={visible}
-          onDismiss={onDismissSnackBar}
+    <ImageBackground
+      source={require('../../assets/fondHome.jpg')}
+      style={styles.backgroundImage}
+    >
+      <TouchableWithoutFeedback onPress={handleOutsidePress}>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === 'ios' ? 'padding' : null}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 120 : 0}
         >
-          {messageError}
-        </Snackbar>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <View style={{ flex: 1, padding: 16 }}>
+              <View style={styles.centerView}>
+                <Avatar.Image
+                  size={100}
+                  source={require('../../assets/profile.jpg')}
+                />
+              </View>
+              <TextInput
+                label="Nom"
+                value={nom}
+                onChangeText={(text) => setNom(text)}
+                mode="outlined"
+                onFocus={() => handleInputFocus('nom')}
+                style={selectedInput === 'nom' ? { borderColor: 'blue' } : null}
+              />
+              <TextInput
+                label="Prénom"
+                value={prenom}
+                onChangeText={(text) => setPrenom(text)}
+                mode="outlined"
+                onFocus={() => handleInputFocus('prenom')}
+                style={
+                  selectedInput === 'prenom' ? { borderColor: 'blue' } : null
+                }
+              />
+              <TextInput
+                label="Email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+                mode="outlined"
+                onFocus={() => handleInputFocus('email')}
+                style={
+                  selectedInput === 'email' ? { borderColor: 'blue' } : null
+                }
+              />
+              <View style={styles.centerButton}>
+                <Button onPress={handleSaveChanges}>
+                  Enregistrer les modifications
+                </Button>
+              </View>
+
+              <TextInput
+                label="Nouveau mot de passe"
+                value={password}
+                onChangeText={handlePasswordChange}
+                mode="outlined"
+                secureTextEntry
+              />
+              {passwordError && (
+                <HelperText type="error" visible={passwordError !== ''}>
+                  {passwordError}
+                </HelperText>
+              )}
+
+              <TextInput
+                label="Confirmez votre mot de passe"
+                value={confirmPassword}
+                onChangeText={handleConfirmPasswordChange}
+                mode="outlined"
+                secureTextEntry
+              />
+              {confirmPasswordError && (
+                <HelperText type="error" visible={confirmPasswordError !== ''}>
+                  {confirmPasswordError}
+                </HelperText>
+              )}
+              <View style={styles.centerButton}>
+                <Button
+                  onPress={handleSavePassword}
+                  style={styles.settingsButton}
+                >
+                  Enregistrer le nouveau mot de passe
+                </Button>
+              </View>
+              <View style={styles.centerButton}>
+                <Button onPress={removeAsyncToken} mode="contained">
+                  Logout
+                </Button>
+              </View>
+            </View>
+          </ScrollView>
+          <Snackbar
+            wrapperStyle={{ top: 0 }}
+            visible={visible}
+            onDismiss={onDismissSnackBar}
+          >
+            {messageError}
+          </Snackbar>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </ImageBackground>
   );
 }
