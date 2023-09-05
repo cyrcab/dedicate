69190644f4d1816865dd403a8ef3fce86762b803
ClementMendes <clement.mendes@epitech.eu>
2023-09-05 16:42:15 +0200

    feat(mobile-app): 🎸 fix bugs on app

--- packages/app-mobile/src/pages/Event.jsx
+++ packages/app-mobile/src/pages/Event.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useRef } from 'react';
-import { ScrollView, StyleSheet, Text, Image, ImageBackground } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  Image,
+  ImageBackground,
+
+} from 'react-native';
 import { axiosApiInstance } from '../../axios.config';
 import { backendUrl } from '../backendUrl';
 import { Button, Card } from 'react-native-paper';
 import { useFocusEffect } from '@react-navigation/native';
+import CloseAuctions from '../components/CloseAuctions';
 
 export default function Event({ navigation }) {
   const [idEvent, setIdEvent] = useState(0);
@@ -23,7 +31,7 @@ export default function Event({ navigation }) {
       .get(backendUrl + 'encheres/' + idEventRef.current)
       .then((response) => {
         setMusic(response.data.votes);
-        setIsActive(response.data.actif.isActive)
+        setIsActive(response.data.actif.isActive);
       })
       .catch((error) => {
         console.log(error);
@@ -82,69 +90,69 @@ export default function Event({ navigation }) {
     }, []),
   );
 
-  console.log(music)
   return (
     <ImageBackground
       source={require('../../assets/fondHome.jpg')}
       style={styles.backgroundImage}
     >
-      <ScrollView contentContainerStyle={styles.container}>
-        {idEvent !== 0 ? (
-          <>
-          {isActive ?
-            (
-              <>
-            <Text style={styles.eventName}>{event.nom}</Text>
+      {idEvent !== 0 ? (
+        <>
+          {isActive ? (
+            <>
+              <ScrollView contentContainerStyle={styles.container}>
+                <Text style={styles.eventName}>{event.nom}</Text>
 
-            {music.map((item, index) => {
-              const isTopTen = index < event.nbSlots;
+                {music.map((item, index) => {
+                  const isTopTen = index < event.nbSlots;
 
-              return (
-                <Card
-                  key={index}
-                  style={[styles.card, isTopTen && styles.topTenCard]}
+                  return (
+                    <Card
+                      key={index}
+                      style={[styles.card, isTopTen && styles.topTenCard]}
+                      onPress={() =>
+                        navigation.navigate('Enchérir', {
+                          event: event,
+                          item: item,
+                        })
+                      }
+                    >
+                      <Card.Title
+                        title={item.Musique.artiste}
+                        subtitle={item.Musique.titre}
+                        left={() => (
+                          <Image
+                            source={{ uri: item.Musique.album }}
+                            style={{ width: 50, height: 50, borderRadius: 10 }}
+                          />
+                        )}
+                        right={() => (
+                          <Text style={{ marginRight: 10 }}>{item.prix}€</Text>
+                        )}
+                      />
+                    </Card>
+                  );
+                })}
+
+                <Button
+                  mode="contained"
+                  style={styles.button}
                   onPress={() =>
-                    navigation.navigate('Enchérir', {
-                      event: event,
-                      item: item,
-                    })
+                    navigation.navigate('Musique', { event: event })
                   }
                 >
-                  <Card.Title
-                    title={item.Musique.artiste}
-                    subtitle={item.Musique.titre}
-                    left={() => (
-                      <Image
-                        source={{ uri: item.Musique.album }}
-                        style={{ width: 50, height: 50, borderRadius: 10 }}
-                      />
-                    )}
-                    right={() => (
-                      <Text style={{ marginRight: 10 }}>{item.prix}€</Text>
-                    )}
-                  />
-                </Card>
-              );
-            })}
-
-            <Button
-              mode="contained"
-              style={styles.button}
-              onPress={() => navigation.navigate('Musique', { event: event })}
-            >
-              Ajoutez un titre
-            </Button>
+                  Ajoutez un titre
+                </Button>
+              </ScrollView>
             </>
-             ): (<Text>salut</Text>)
-
-            }
-          </>
-        ) : (
-          <Text style={styles.noEventMessage}>
-            Vous n'avez aucun événement actif
-          </Text>
-        )}
-      </ScrollView>
+          ) : (
+              <CloseAuctions idEventRef={idEventRef.current} />
+          )}
+        </>
+      ) : (
+        <Text style={styles.noEventMessage}>
+          Vous n'avez aucun événement actif
+        </Text>
+      )}
     </ImageBackground>
   );
 }
