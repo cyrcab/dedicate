d1d62ee482333437d3677639a26766482493e579
Chachafouineuse <aurelien.schmit@outlook.fr>
2023-09-18 15:28:05 +0200

    fix(mobile-app): 🐛 bugs on rebase

--- packages/app-mobile/src/navigation/AppNavigator.jsx
+++ packages/app-mobile/src/navigation/AppNavigator.jsx
@@ -13,15 +13,6 @@ import SlotInformation from '../pages/SlotInformation';
 import Auctions from '../pages/Auctions';
 
 import Wallet from '../pages/Wallet/Wallet';
-import AddDediCoin from '../pages/Wallet/AddDediCoin';
-import BankDetails from '../pages/Wallet/BankDetails';
-import PaymentMethodForm from '../pages/Wallet/PaymentMethodForm';
-
-import Wallet from "../pages/Wallet/Wallet";
-
-
-import PaymentMethodForm from "../pages/Wallet/PaymentMethodForm";
-import StripeCheckout from "../pages/Wallet/StripeCheckout";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -72,10 +63,6 @@ const WalletStack = () => (
       component={Wallet}
       options={{ headerShown: false }}
     />
-    <Stack.Screen name="Ajouter" component={AddDediCoin} />
-    <Stack.Screen name="Bank Details" component={BankDetails} />
-    <Stack.Screen name="PaymentMethodForm" component={PaymentMethodForm} />
-    <Stack.Screen name="StripeCheckout" component={StripeCheckout} />
   </Stack.Navigator>
 );
 
